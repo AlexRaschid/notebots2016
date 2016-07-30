6e14a3e4421a53588ab8f22b872e08798b76d5e6
Alex R <alexraschid1337@gmail.com>
2016-07-30 14:38:27 -0400

    First Commit

---
+++ lcd.js
@@ -0,0 +1,15 @@
+var five = require("johnny-five");
+var Tessel = require("tessel-io");
+var board = new five.Board({
+  io: new Tessel()
+});
+
+board.on("ready", () => {
+  var lcd = new five.LCD({
+    pins: ["a2", "a3", "a4", "a5", "a6", "a7"]
+  });
+
+  lcd.cursor(0, 0).print("Get Memed Bro");
+  lcd.cursor(1, 0).print("O.O" + " ;)");
+});
+
